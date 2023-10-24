1a5555a6e4c648eca7530fe21568566c57c3d956
wakayinja frank <142891493+wakayinjaF@users.noreply.github.com>
2023-10-24 14:34:56 +0300

    Update wordcloudkyrus2.py

--- wordcloudkyrus2.py
+++ wordcloudkyrus2.py
@@ -6,8 +6,8 @@ import base64
 from io import BytesIO
 
 # Spotify API credentials
-CLIENT_ID = "4656a4330aa14b26aa5103670483668a"
-CLIENT_SECRET = "ae442d65b8fa4538bb6ab92531bf5ad8"
+CLIENT_ID = "put client_id"
+CLIENT_SECRET = "put client_secret"
 
 # Playlist ID
 playlist_id = '6Wc2THamjv092cevwohMyt'
